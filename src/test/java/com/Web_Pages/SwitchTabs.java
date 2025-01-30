71470953e84c62fca8c0181616c06feab562ea56
Tuna-testing <testing@thetunagroup.com>
2025-01-31 02:31:39 +0530

    Initial commit

---
+++ src/test/java/com/Web_Pages/SwitchTabs.java
@@ -0,0 +1,17 @@
+package com.Web_Pages;
+
+import com.Web_Utils.BaseClassWeb;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class SwitchTabs extends BaseClassWeb {
+    @FindBy(xpath = "//a[@aria-label='Go to Six Flags Home']//img[1]")
+    private WebElement backToHome;
+
+
+    public SwitchTabs(WebDriver webDriver) {
+        PageFactory.initElements(driver.get(), this);
+    }
+}
