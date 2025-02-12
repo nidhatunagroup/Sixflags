f7839a45d3e066ed9d2e2ccedeac70619b6096fe
Rahul dev <dev@thetunagroup.com>
2025-02-12 19:09:41 +0530

    signup page commit

---
+++ src/test/java/com/Web_Test/SignUpTest.java
@@ -0,0 +1,11 @@
+package com.Web_Test;
+
+import com.Web_Utils.BaseClassWeb;
+import org.testng.annotations.Test;
+
+public class SignUpTest extends BaseClassWeb {
+    @Test
+    public void testSignUpNewUser(){
+
+    }
+}
